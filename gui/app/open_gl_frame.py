3344c97cf293f2bb2dc167a27ba9e4718485c026
joegasewicz <joegasewicz@gmail.com>
2025-02-25 22:50:52 +0000

    initial

--- gui/app/open_gl_frame.py
+++
@@ -1,9 +0,0 @@
-import wx
-from app.canvas import JewelCanvas
-
-
-class OpenGLFrame(wx.Frame):
-
-    def __init__(self):
-        super().__init__(None, title="Jewel App", size=(960, 720))
-        canvas = JewelCanvas(self)
--- gui/jewelapp/src/jewelapp/app.py
+++
@@ -1,8 +0,0 @@
-"""
-My first application
-"""
-
-
-def main():
-    # Your app logic goes here
-    print("Hello, World.")
